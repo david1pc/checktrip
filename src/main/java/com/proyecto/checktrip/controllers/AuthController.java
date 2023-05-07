ad61bd524b600d31c0a1a5c0a51962cf6ef2ccca
David <davidalejandrorh@hotmail.com>
2023-05-06 20:15:16 -0500

    Se modifica configuracion de authcontroller

--- src/main/java/com/proyecto/checktrip/controllers/AuthController.java
+++ src/main/java/com/proyecto/checktrip/controllers/AuthController.java
@@ -19,6 +19,7 @@ import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AuthController {
     private final ClientServiceImpl clientService;
